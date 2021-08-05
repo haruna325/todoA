c8fa8d339e485fc989c4540658e616414ec1111f
rksYuta <kobayashi.yuta.rks@gmail.com>
2021-08-05 16:59:59 +0900

    画面編集しました　小林

---
+++ src/views/AddHome.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+    <Add />
+    </div>
+</template>
+
+<script>
+import Add from '../components/Add.vue'
+
+export default ({
+    name: 'AddHome',
+    components:{
+        Add
+    }
+})
+</script>
