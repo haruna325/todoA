d63041f8b30ede689ef472a8198ee0c6a6718b6c
rksYuta <kobayashi.yuta.rks@gmail.com>
2021-08-05 22:35:45 +0900

    追加画面の作成・一覧画面の作成（仮）

--- src/components/Add.vue
+++ src/components/Add.vue
@@ -1,3 +1,51 @@
 <template>
-    <h1>追加</h1>
+    <div class="add">
+    <h1>追加画面</h1>
+    <form>
+        <!-- タイトルは変更できない -->
+        <div>タイトル：<input type="text" v-model="title"></div>
+        <div>内容：<textarea v-model="about"></textarea></div>
+        <div>期日：<input type="text" v-mode="deadline"></div>
+        <!-- selectタグにv-modelつける -->
+        <div>進捗率：<select v-model="progress">
+                        <option>0</option>
+                        <option>10％</option>
+                        <option>20％</option>
+                        <option>30％</option>
+                        <option>40％</option>
+                        <option>50％</option>
+                        <option>60％</option>
+                        <option>70％</option>
+                        <option>80％</option>
+                        <option>90％</option>
+                        <option>100％</option>
+                    </select></div>
+        <!-- 期日：<input type="date" v-model="deadline"> -->
+        メモ：<textarea v-model="memo"></textarea>
+        <div><button type="button" @click="addtodo">キャンセル</button> ｜
+        <!-- clickしたらmethods(入力した値を一覧画面に追加、追加後一覧画面に遷移)呼び出す-->
+        <button type="button">追加</button></div>
+    </form>
+    </div>
 </template>
+<script>
+export default ({
+    // 何も入力されていなかったら追加できない
+    // 追加後一覧画面に遷移
+    data(){
+        return{
+            title:'',
+            about:'',
+            deadline:'',
+            progress:'',
+            memo:''
+        }
+    },
+    methods:{
+        addtodo(){
+
+        }
+    }
+
+})
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,37 @@
 <template>
   <div class="about">
     <h1>TODO一覧</h1>
+    <table border=1>
+      <thead>
+        <tr>
+          <th>制作日時</th>
+          <th>タイトル</th>
+          <th>内容</th>
+          <th>期日</th>
+          <th>進捗率0-100</th>
+          <th>メモ</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="todo in todos" :key='todo'>
+          <td>{{todo.date}}</td>
+          <td>{{todo.title}}</td>
+          <td>{{todo.about}}</td>
+          <td>{{todo.deadline}}</td>
+          <td>{{todo.progress}}</td>
+          <td>{{todo.memo}}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
+
 </template>
+<script>
+export default{
+  data(){
+    return{
+      todos:[{date:'2021/8/5',title:'サンプル1',about:'gitの勉強',deadline:'2021/8/10',progress:'20%',memo:'復習する'}]
+    }
+  }
+}
+</script>
